116c21ba6c89755a292bb350522786940bf3cdb3
Roman Filipenko <romajz21@gmail.com>
2020-07-03 12:51:09 +0300

    Initial Commit

---
+++ GeoPostTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoPostTest
+//
+//  Created by Roma Filipenko on 03.07.2020.
+//  Copyright © 2020 Roma&Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
